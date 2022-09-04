8796d82dbeffbb64d606729be0e1cea090f82be0
ConteMan <real.conteman@gmail.com>
2022-09-04 12:04:22 +0800

    feat: modify vue-router config;

--- src/router.d.ts
+++ src/router.d.ts
@@ -2,9 +2,7 @@ import 'vue-router'
 
 declare module 'vue-router' {
   interface RouteMeta {
-    // 是可选的
-    title?: string
-    // 每个路由都必须声明
-    keepAlive: boolean
+    title: string
+    keepAlive?: boolean
   }
 }
